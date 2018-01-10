03de3254ca66750108dddf9d54b60b919cad456a
rshanedev <ryansshane@rocketmail.com>
2018-01-10 10:25:56 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
+import org.bouncycastle.jcajce.provider.digest.SHA3;
+import org.bouncycastle.util.encoders.Hex;
+import org.junit.Test;
 
 public Class Main {
   public static void main(String[] args){
-    
+    //just hashing "Hello world" right now...
+    String input = "Hello world";
+    SHA3.DigestSHA3 digestSHA3 = new SHA3.Digest512();
+    byte[] digest = digestSHA3.digest(input.getBytes());
+
+    System.out.println("SHA3-512 = " + Hex.toHexString(digest));
   }
 }
