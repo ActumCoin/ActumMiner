7025d38b2519295ac22bb631678069bcfa12cc66
rshanedev <ryansshane@rocketmail.com>
2018-01-11 15:50:52 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,10 +3,14 @@ import java.util.Timer;
 import org.bouncycastle.jcajce.provider.digest.SHA3;
 import org.bouncycastle.util.encoders.Hex;
 
-import mining.Heartbeat;
+import mining.BlockchainCommand;
+import mining.Heartbeat
 
 public class Main {
   public static void main(String[] args){
+    //init
+    BlockchainCommand bc = new BlockchainCommand();
+    
     //just hashing "Hello world" right now...
     String input = "Hello world";
     SHA3.DigestSHA3 digestSHA3 = new SHA3.Digest512();
