a994399f62007a8da7612f0c9283c963e9465efa
rshanedev <ryansshane@rocketmail.com>
2018-01-10 19:50:09 -0500

    Create Heartbeat.java

---
+++ mining/Heartbeat.java
@@ -0,0 +1,12 @@
+package mining;
+
+import java.util.TimerTask;
+
+public class Heartbeat extends TimerTask {
+	private static final String EMPTY = "";
+
+	@Override
+	public void run() {
+		//mine empty transaction here
+	}
+}
