0ff96ee842f0b33844bbcf51fe1bdb88aa2a3807
rshanedev <ryansshane@rocketmail.com>
2018-01-11 15:56:09 -0500

    Update BlockchainCommand.java

--- mining/BlockchainCommand.java
+++ mining/BlockchainCommand.java
@@ -25,12 +25,49 @@ public class BlockchainCommand {
 			// same as d
 			finalCommand = "multichaind ";
 		}
+		
 		finalCommand += command;
+		
 		try {
 			Process p = rt.exec(finalCommand);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public String e(String to, String command, boolean getReturn) {
+		String finalCommand = "";
+		switch (to) {
+		case "d":
+			finalCommand = "multichaind ";
+			break;
+		case "u":
+			finalCommand = "multichain-util ";
+			break;
+		case "c":
+			finalCommand = "multichain-cli ";
+			break;
+		case "o":
+			finalCommand = "multichain-cold ";
+			break;
+		default:
+			// same as d
+			finalCommand = "multichaind ";
+		}
+		
+		finalCommand += command;
+		Process p = null;
+		try {
+			p = rt.exec(finalCommand);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		if (getReturn) {
+			return p.getInputStream().toString();
+		}
+		
+		return null;
 	}
 
 }
