202bdfd9c9c0a02745c9641a45f0ced352c1d73d
rshanedev <ryansshane@rocketmail.com>
2018-01-12 18:55:33 -0500

    Create XMLReaderWriter.java

---
+++ util/XMLReaderWriter.java
@@ -0,0 +1,114 @@
+package util;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class XMLReaderWriter {
+	
+	private static String address = null;
+	private static ArrayList<String> rolev;
+
+	public static boolean readXML(String xml) {
+		rolev = new ArrayList<String>();
+		Document dom;
+		// Make an instance of the DocumentBuilderFactory
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		try {
+			// use the factory to take an instance of the document builder
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			// parse using the builder to get the DOM mapping of the
+			// XML file
+			dom = db.parse(xml);
+
+			Element doc = dom.getDocumentElement();
+
+			address = getTextValue(address, doc, "address");
+			System.out.println(address);
+			if (address != null) {
+				if (!address.isEmpty())
+					rolev.add(address);
+			}
+			return true;
+
+		} catch (ParserConfigurationException pce) {
+			System.out.println(pce.getMessage());
+		} catch (SAXException se) {
+			System.out.println(se.getMessage());
+		} catch (IOException ioe) {
+			System.err.println(ioe.getMessage());
+		}
+
+		return false;
+	}
+
+	public static void saveToXML(String xml) {
+		Document dom;
+		Element e = null;
+
+		// instance of a DocumentBuilderFactory
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		try {
+			// use factory to get an instance of document builder
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			// create instance of DOM
+			dom = db.newDocument();
+
+			// create data elements and place them under root
+			e = dom.createElement("address");
+			e.appendChild(dom.createTextNode(address));
+
+			dom.appendChild(e);
+
+			try {
+				Transformer tr = TransformerFactory.newInstance().newTransformer();
+				tr.setOutputProperty(OutputKeys.INDENT, "yes");
+				tr.setOutputProperty(OutputKeys.METHOD, "xml");
+				tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+				tr.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "roles.dtd");
+				tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+				// send DOM to file
+				tr.transform(new DOMSource(dom), new StreamResult(new FileOutputStream(xml)));
+
+			} catch (TransformerException te) {
+				System.out.println(te.getMessage());
+			} catch (IOException ioe) {
+				System.out.println(ioe.getMessage());
+			}
+		} catch (ParserConfigurationException pce) {
+			System.out.println("UsersXML: Error trying to instantiate DocumentBuilder " + pce);
+		}
+	}
+
+	private static String getTextValue(String def, Element doc, String tag) {
+		String value = def;
+		NodeList nl;
+		nl = doc.getElementsByTagName(tag);
+		if (nl.getLength() > 0 && nl.item(0).hasChildNodes()) {
+			value = nl.item(0).getFirstChild().getNodeValue();
+		}
+		return value;
+	}
+
+	public static String getAddress() {
+		return address;
+	}
+	
+
+}
