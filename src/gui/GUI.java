ace9a6a4e7dde84a37ef537d1af6c484c69dcda1
rshanedev <ryansshane@rocketmail.com>
2018-01-16 16:33:12 -0500

    Update and rename src/util/GUI.java to src/gui/GUI.java

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,16 +1,23 @@
-package util;
+package gui;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.event.*;
 import java.util.Enumeration;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.*;
 
+import main.Main;
 import mining.MiningManager;
+import util.Preferences;
 
 public class GUI extends JFrame {
-	JFrame f;
+	private JFrame f;
+	private boolean isPreferences = false;
 
 	public GUI() {
 		// init
@@ -18,8 +25,8 @@ public class GUI extends JFrame {
 		this.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
 
 		// logo
-		JLabel logo = new JLabel(new ImageIcon("logo.png"));
-		logo.setBounds(274, 10, 171, 119);// gah! not quite centered, should be right another 0.5 pixels
+		JLabel logo = new JLabel(new ImageIcon("C:\\xampp\\htdocs\\ActumCoin\\logo.png"));
+		logo.setBounds(274, 0, 171, 119);// gah! not quite centered, should be right another 0.5 pixels
 
 		// mine button
 		JButton mineButton = new JButton("Start Mining");
@@ -31,6 +38,16 @@ public class GUI extends JFrame {
 		preferencesButton.setBounds(480, 10, 210, 40);
 		preferencesButton.setBackground(Color.WHITE);
 
+		// preferences stuff
+		CheckBox linkCheckBox = new CheckBox("Link with wallet", Preferences.isLink());
+		linkCheckBox.setBounds(480, 60, 200, 30);
+		linkCheckBox.setVisible(false);
+		linkCheckBox.setToolTipText("This allows ActumMiner to automatically sync with your ActumWallet, if it's on this PC.");
+		CheckBox publicStatsCheckBox = new CheckBox("Public stats", Preferences.isPublicStats());
+		publicStatsCheckBox.setBounds(480, 100, 200, 30);
+		publicStatsCheckBox.setVisible(false);
+		publicStatsCheckBox.setToolTipText("This allows anyone to view your stats on actumcrypto.org/mine-stats.");
+		
 		// address button
 		JButton addressButton = new JButton("Set Address");
 		addressButton.setBounds(10, 10, 210, 40);
@@ -38,7 +55,7 @@ public class GUI extends JFrame {
 
 		// display current address
 		JLabel currentAddress = new JLabel(Preferences.getAddress());
-		currentAddress.setBounds(10, 10, 100, 100);
+		currentAddress.setBounds(10, 10, 210, 100);
 		currentAddress.setFont(new javax.swing.plaf.FontUIResource("1234", Font.ITALIC, 14));
 
 		// button listeners
@@ -52,6 +69,24 @@ public class GUI extends JFrame {
 					mineButton.setText("Stop Mining");
 					MiningManager.mine();
 				}
+			}
+		});
+
+		preferencesButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				isPreferences = !isPreferences;
+				linkCheckBox.setVisible(isPreferences);
+				publicStatsCheckBox.setVisible(isPreferences);
+				if (isPreferences) {
+					// if already closed
+					preferencesButton.setText("Save");
+				} else {
+					// if already open\
+					Preferences.setLink(linkCheckBox.isChecked());
+					Preferences.setPublicStats(publicStatsCheckBox.isChecked());
+					preferencesButton.setText("Preferences");
+				}
+				
 			}
 		});
 
@@ -70,7 +105,7 @@ public class GUI extends JFrame {
 		// window close listener
 		this.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
-				if (MiningManager.isCurrentlyMining()) {
+				if (MiningManager.isCurrentlyMining() ) {
 					int n = JOptionPane.showConfirmDialog(f,
 							"You are currently mining, would you like to stop and exit?", "Currently mining",
 							JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
@@ -87,6 +122,8 @@ public class GUI extends JFrame {
 		add(logo);
 		add(mineButton);
 		add(preferencesButton);
+		add(linkCheckBox);
+		add(publicStatsCheckBox);
 		add(addressButton);
 		add(currentAddress);
 
@@ -104,6 +141,7 @@ public class GUI extends JFrame {
 			if (value instanceof javax.swing.plaf.FontUIResource)
 				UIManager.put(key, f);
 		}
+		UIManager.put("ToolTip.font", new javax.swing.plaf.FontUIResource("1234", Font.ITALIC, 14));
 	}
 
 }
