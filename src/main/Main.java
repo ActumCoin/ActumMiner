4567ce5cbe6c8455483577d3860101b3c71b2933
rshanedev <ryansshane@rocketmail.com>
2018-01-19 20:26:59 -0500

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,25 +1,37 @@
 package main;
 
+import java.nio.ByteBuffer;
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.bouncycastle.jcajce.provider.digest.SHA3;
 import org.bouncycastle.util.encoders.Hex;
 
 import gui.GUI;
+import util.FourWords;
 import util.Preferences;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		// init
-		Preferences p = new Preferences();
-		
-		System.out.println(p.address);
-		
-		// just hashing "Hello world" right now...
-		String input = "Hello world";
-		SHA3.DigestSHA3 digestSHA3 = new SHA3.Digest512();
-		byte[] digest = digestSHA3.digest(input.getBytes());
+		new Preferences();
+		new GUI();
+		SHA3.DigestSHA3 digestSHA3 = new SHA3.Digest256();
 
-		System.out.println("SHA3-512 = " + Hex.toHexString(digest));
+		String[] IDSet = Preferences.getIDSet();
+		if (IDSet[0] == "" || IDSet[1] == "") {
+			if (IDSet[0] == "") {
+				byte[] d = digestSHA3.digest(ByteBuffer.allocate(4).putInt(ThreadLocalRandom.current().nextInt(0, 2147483647)).array());
+				IDSet[0] = Hex.toHexString(d);
+			}
+			if (IDSet[1] == "") {
+				String s = FourWords.generate();
+				IDSet[1] = s;
+			}
+			Preferences.setIDSet(IDSet);
+		}
+		
+		System.out.println(IDSet[0] + "\n" + IDSet[1]);
 
 	}
 	
