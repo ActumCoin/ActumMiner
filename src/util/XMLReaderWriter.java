ea1d8b6af10cbadafbe1e0a4180231c518cece6e
rshanedev <ryansshane@rocketmail.com>
2018-01-19 20:28:34 -0500

    Update XMLReaderWriter.java

--- src/util/XMLReaderWriter.java
+++ src/util/XMLReaderWriter.java
@@ -15,6 +15,8 @@ import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class XMLReaderWriter {
