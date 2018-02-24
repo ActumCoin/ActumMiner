939b8c696c830e0c456494b3a675c5f496a8388d
rshanedev <ryansshane@rocketmail.com>
2018-02-23 20:02:01 -0500

    Update MiningManager.java

--- src/mining/MiningManager.java
+++ src/mining/MiningManager.java
@@ -1,21 +1,11 @@
 package mining;
 
 import java.io.IOException;
-import java.net.*;
 
 public class MiningManager {
 	private static boolean currentlyMining;
-	private URL url;
-	private URLConnection con;
-	private HttpURLConnection http;
 
 	private MiningManager() throws IOException {
-		// setup connection to controlling server
-		url = new URL("didnt.buy.domain/yet.so?this=is-placeholder");
-		con = url.openConnection();
-		http = (HttpURLConnection) con;
-		http.setRequestMethod("POST");
-		http.setDoOutput(true);
 	}
 
 	public static boolean isCurrentlyMining() {
