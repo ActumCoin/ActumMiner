d54b756324d9007f1ab55fafe4165f9b83afbc3c
codegreen <ryansshane@rocketmail.com>
2018-02-28 15:58:17 -0500

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,38 +1,50 @@
 package main;
 
-import java.nio.ByteBuffer;
-import java.util.concurrent.ThreadLocalRandom;
-
-import org.bouncycastle.jcajce.provider.digest.SHA3;
-import org.bouncycastle.util.encoders.Hex;
+import java.util.List;
 
 import gui.GUI;
-import util.FourWords;
+import mining.BalanceManager;
+import mining.ForwardSmartContract;
+import mining.MiningManager;
+import multichain.command.MultiChainCommand;
+import multichain.command.MultichainException;
 import util.Preferences;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		// init
-		new Preferences();
-		
-		SHA3.DigestSHA3 digestSHA3 = new SHA3.Digest256();
 
-		String[] IDSet = Preferences.getIDSet();
-		if (IDSet[0] == "" || IDSet[1] == "") {
-			if (IDSet[0] == "") {
-				byte[] d = digestSHA3.digest(ByteBuffer.allocate(4).putInt(ThreadLocalRandom.current().nextInt(0, 2147483647)).array());
-				IDSet[0] = Hex.toHexString(d);
-			}
-			if (IDSet[1] == "") {
-				String s = FourWords.generate();
-				IDSet[1] = s;
-			}
-			Preferences.setIDSet(IDSet);
+	public static void main(String[] args) throws InterruptedException, MultichainException {
+		new Preferences();
+		// connect to blockchain
+		String ip = "localhost";
+		String port = "7440";
+		String user = "multichainrpc";
+		String pass = "password";
+		MultiChainCommand m = new MultiChainCommand(ip, port, user, pass);
+
+		// get address
+		List<String> addresses = null;
+		try {
+			addresses = m.getAddressCommand().getAddresses();
+		} catch (MultichainException e) {
+			e.printStackTrace();
 		}
-		
+
+		String address = null;
+
+		for (String a : addresses) {
+			address = a;
+		}
+
+		MiningManager.createInstance(m);
+		BalanceManager.createInstance(m, address);
+
 		new GUI();
+		System.out.println(Preferences.getAddress());
+		if (!Preferences.getAddress().isEmpty()) {
+			ForwardSmartContract.setInstance(address, Preferences.getAddress());
+			ForwardSmartContract.getInstance().thread.start();
+		}
 
 	}
-	
+
 }
