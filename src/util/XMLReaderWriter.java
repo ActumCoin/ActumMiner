50c90d7706566756f9dede32588aaa8a97ff1202
codegreen <ryansshane@rocketmail.com>
2018-02-28 15:59:57 -0500

    Update XMLReaderWriter.java

--- src/util/XMLReaderWriter.java
+++ src/util/XMLReaderWriter.java
@@ -22,10 +22,6 @@ import org.xml.sax.SAXException;
 public class XMLReaderWriter {
 
 	protected static String address = null;
-	protected static boolean link;
-	protected static boolean publicStats;
-	protected static String id = null;
-	protected static String secret = null;
 
 	public static boolean readXML(String xml) {
 		Document dom;
@@ -39,21 +35,6 @@ public class XMLReaderWriter {
 
 			// get address
 			address = getTextValue(doc, "address");
-
-			// get link
-			link = Boolean.parseBoolean(getTextValue(doc, "link"));
-
-			// get public-stats
-			publicStats = Boolean.parseBoolean(getTextValue(doc, "public-stats"));
-
-			// id element
-			Element idE = getChild(doc, "id");
-			
-			// get id
-			id = idE.getAttribute("id");
-
-			// get secret
-			secret = idE.getAttribute("secret");
 
 			return true;
 
@@ -72,9 +53,6 @@ public class XMLReaderWriter {
 		Document dom;
 		Element e = null;
 		Element aE = null;
-		Element lE = null;
-		Element pE = null;
-		Element idE = null;
 
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		try {
@@ -87,27 +65,11 @@ public class XMLReaderWriter {
 			// create address element
 			aE = dom.createElement("address");
 
-			// create link element
-			lE = dom.createElement("link");
-
-			// create public-stats element
-			pE = dom.createElement("public-stats");
-			
-			// create id element
-			idE = dom.createElement("id");
-
 			// add elements to root element
 			e.appendChild(aE);
-			e.appendChild(lE);
-			e.appendChild(pE);
-			e.appendChild(idE);
 
 			// add preferences data
 			aE.appendChild(dom.createTextNode(address));
-			lE.appendChild(dom.createTextNode(Boolean.toString(link)));
-			pE.appendChild(dom.createTextNode(Boolean.toString(publicStats)));
-			idE.setAttribute("id", id);
-			idE.setAttribute("secret", secret);
 
 			dom.appendChild(e);
 
