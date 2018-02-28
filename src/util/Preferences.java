06b5666d8c40f2bd37d5adb85ab66950a2241232
codegreen <ryansshane@rocketmail.com>
2018-02-28 15:59:32 -0500

    Update Preferences.java

--- src/util/Preferences.java
+++ src/util/Preferences.java
@@ -11,37 +11,8 @@ public class Preferences extends XMLReaderWriter {
 		return address;
 	}
 	
-	public static boolean isLink() {
-		return link;
-	}
-	
-	public static boolean isPublicStats() {
-		return publicStats;
-	}
-	
-	public static String[] getIDSet() {
-		String[] IDSet = {id, secret};
-		return IDSet;
-	}
-	
 	public static void setAddress(String a) {
 		address = a;
-		writeXML(ADDRESS);
-	}
-	
-	public static void setLink(boolean l) {
-		link = l;
-		writeXML(ADDRESS);
-	}
-
-	public static void setPublicStats(boolean p) {
-		publicStats = p;
-		writeXML(ADDRESS);
-	}
-	
-	public static void setIDSet(String[] s) {
-		id = s[0];
-		secret = s[1];
 		writeXML(ADDRESS);
 	}
 	
